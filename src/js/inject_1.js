83e6b81e650a219d5ffce61a47d5bf524cf9391e
TakiVotoid <895878439@qq.com>
2019-08-16 21:01:15 +0800

    Delete inject_1.js

--- src/js/inject_1.js
+++
@@ -1,6 +0,0 @@
-var inj_1 = document.createElement('script');
-inj_1.src = chrome.extension.getURL('js/quick_finish.js');
-inj_1.onload = function() {
-	this.parentNode.removeChild(this);
-};
-(document.head || document.documentElement).appendChild(inj_1);
